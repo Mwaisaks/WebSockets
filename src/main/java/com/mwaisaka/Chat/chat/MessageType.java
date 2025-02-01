910eaa8e536fd65be68dacdca66d0ec3bdcf0c45
Phenny <mwaisakaphenny@gmail.com>
2025-02-01 12:45:32 +0300

    Implemented the backend of the chat application

---
+++ src/main/java/com/mwaisaka/Chat/chat/MessageType.java
@@ -0,0 +1,9 @@
+package com.mwaisaka.Chat.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+
+}
--- src/main/java/com/mwaisaka/Chat/config/WebSocketConfig.java
+++ src/main/java/com/mwaisaka/Chat/config/WebSocketConfig.java
@@ -1,7 +1,26 @@
 package com.mwaisaka.Chat.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 @Configuration
 @EnableWebSocketMessageBroker
-
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+       //adding a new stomp endpoint to our websocket configuration
+        registry.addEndpoint("/ws").withSockJS();
+
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //Enable the message broker
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
 }
